892a3a5e556722dbfc28781446db5e0198e1abe5
jairorjuela <genius85862@gmail.com>
2018-09-02 10:41:37 -0500

    Estados y componentes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import Welcome from './Welcome';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: "Hola mundo desde estado"
+    }
+  }
   render() {
-    const names = ["Jairo", "Andres", "Orjuela", "Hower"];
+    // const names = ["Jairo", "Andres", "Orjuela", "Hower"];
     return (
+      // <div>
+      // {names.map(name =>
+      //   <Welcome name= {name}/>
+      // )}
+      // </div>
       <div>
-        <h1>Hola Mundo</h1>
-        <ul>
-          {names.map(name =>
-            <li>{name}</li>
-          )}
-        </ul>
-      </div>        
+        <h1>{this.state.title}</h1>
+        <button onClick={this.changeTitle.bind(this)}>Cambia el titulo</button>
+      </div>
     );
+  }
+
+  changeTitle(){
+    this.setState({
+      title: "Nuevo titulo"
+    });
   }
 }
 
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Welcome = (props) => <h1> Hola {props.name} desde Component</h1>;
+
+export default Welcome;
