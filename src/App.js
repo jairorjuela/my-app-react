ef58ddd2d81d78832a68e8ca8d2cce5d315d04da
jairorjuela <genius85862@gmail.com>
2018-09-02 20:53:51 -0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
 import React, { Component } from 'react';
-import Welcome from './Welcome';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
+
     this.state = {
-      name: "",
-      terms: false
-    }
+      names: []
+    };
+
+    axios.get("http://localhost:3001/names")
+      .then(response => {
+        this.setState({
+          names: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
   render() {
     return (
       <div>
         <h1>Hola Mundo</h1>
-        <input type="text" value={this.state.name} onChange={this.updateName.bind(this)} />
-        <div>
-          <label>
-            <input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)} /> Acepto los terminos
-          </label>
-        </div>
-        <button onClick={this.sayHi.bind(this)}>Say Hi!</button>
+        <ul>
+          {this.state.names.map(name =>
+            <li key={name}>{name}</li>
+          )}
+        </ul>
       </div>
     );
   }
 
-  updateName(event){
-    this.setState({
-      name: event.target.value
-    });
-  }
-
-  updateTerms(event){
-    this.setState({
-      terms: event.target.checked
-    });
-  }
-
-  sayHi(){
-    if(this.state.terms){
-      alert('Hola ' + this.state.name);
-    }else {
-      alert('Acepta terminos')
-    }
-  }
 
 
 }
